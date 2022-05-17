235f3d715d95733b6626eab7d0bce46bb3e6130c
Vivek jha <85884487+duckling69@users.noreply.github.com>
2022-05-17 17:56:40 +0530

    Add files via upload

---
+++ precache-manifest.1973d3605292fd7b4e5e6fb9af79f2ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02709863e22589273130525ba387c51e",
+    "url": "/CountdownTimer/index.html"
+  },
+  {
+    "revision": "3564fec6ab77c92ca00c",
+    "url": "/CountdownTimer/static/css/main.95a97fae.chunk.css"
+  },
+  {
+    "revision": "a83b866c1c0f576d1986",
+    "url": "/CountdownTimer/static/js/2.5b385fb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CountdownTimer/static/js/2.5b385fb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3564fec6ab77c92ca00c",
+    "url": "/CountdownTimer/static/js/main.c1d5a311.chunk.js"
+  },
+  {
+    "revision": "5b66cdde8c2fcf171013",
+    "url": "/CountdownTimer/static/js/runtime-main.49463908.js"
+  }
+]);
